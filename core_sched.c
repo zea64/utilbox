74d0d01c01ab25280cfe32c4075270a218522b74
Natalya McKay <zea_64@proton.me>
2025-11-22 10:36:06 -0500

    Let core_sched take a command argument to exec, useful for pipelines.

--- core_sched.c
+++ core_sched.c
@@ -2,27 +2,44 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <sys/prctl.h>
+#include <unistd.h>
 
-static const char USAGE[] = "USAGE: core_sched <TID>\n";
+static const char USAGE[] = "USAGE: core_sched <TID>|<CMD>\n";
 
 int core_sched(int argc, char** argv) {
-	ASSERT(argc == 1, USAGE);
+	ASSERT(argc >= 1, USAGE);
 
 	char* endptr;
 	int tid = strtol(argv[0], &endptr, 10);
-	ASSERT(*endptr == '\0', "Invalid tid\n");
 
-	ASSERT(
-		prctl(
-			PR_SCHED_CORE,
-			PR_SCHED_CORE_CREATE,
-			tid,
-			PR_SCHED_CORE_SCOPE_THREAD,
-			0
-		) != -1,
-		"prctl: %s\n",
-		strerror(errno)
-	);
+	if (*endptr == '\0') { // TID case
+		ASSERT(argc == 1, USAGE);
+		ASSERT(
+			prctl(
+				PR_SCHED_CORE,
+				PR_SCHED_CORE_CREATE,
+				tid,
+				PR_SCHED_CORE_SCOPE_THREAD,
+				0
+			) != -1,
+			"prctl: %s\n",
+			strerror(errno)
+		);
+	} else { // CMD case
+		ASSERT(
+			prctl(
+				PR_SCHED_CORE,
+				PR_SCHED_CORE_CREATE,
+				0,
+				PR_SCHED_CORE_SCOPE_THREAD_GROUP,
+				0
+			) != -1,
+			"prctl: %s\n",
+			strerror(errno)
+		);
+		execvp(argv[0], argv);
+		ASSERT(0, "execvp: %s\n", strerror(errno));
+	}
 
 	return EXIT_SUCCESS;
 }
